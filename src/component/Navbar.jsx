d563f2442378a8ffd642414cc1f5f99145d4405c
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-08-14 23:09:21 +0530

    fix-some-issue

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "../component/Navbar.css";
 import img from "../images/basketimage.jpg";
-// import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasketIcon';
-//import Button from "@material-ui/core/Button";
-//import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Navbar = () => {
   return (
@@ -12,13 +9,7 @@ const Navbar = () => {
         <div>
           {/* <ShoppingBasketIcon /> */}
           <img className="image" src={img} alt=" " />
-
-          {/* <ShoppingBasketIcon /> */}
-
           <h1 id="text">Hello Basket!</h1>
-          {/* <li className="list-item">here are basket image</li>
-                <li className="list-item">here are basket image</li>
-                <li className="list-item">here are basket image</li> */}
         </div>
       </div>
     </>
